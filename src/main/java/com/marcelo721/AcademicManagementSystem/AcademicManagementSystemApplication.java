3298fced114d2583a47f7f1a2b2f0faeb1d8cd23
marcelo721 <marceloh.sousa@alu.ufc.br>
2025-06-30 09:22:15 -0300

    first commit

---
+++ src/main/java/com/marcelo721/AcademicManagementSystem/AcademicManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo721.AcademicManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelo721/AcademicManagementSystem/AcademicManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelo721.AcademicManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
